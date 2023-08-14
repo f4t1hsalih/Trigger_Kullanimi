b0daa48c121eb871023938cffbec3c6bab330db6
f4t1hsalih <f4t1hsalih@gmail.com>
2023-08-15 01:20:05 +0300

    DersListele Metodu yazıldı

--- Trigger_Kullanimi/Form1.Designer.cs
+++ Trigger_Kullanimi/Form1.Designer.cs
@@ -28,26 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             groupBox1 = new GroupBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            txtid = new TextBox();
-            txtAd = new TextBox();
-            txtYazar = new TextBox();
-            txtSayfa = new TextBox();
-            txtYayinevi = new TextBox();
-            txtTur = new TextBox();
-            btnEkle = new Button();
             btnSil = new Button();
+            btnEkle = new Button();
+            txtTur = new TextBox();
+            txtYayinevi = new TextBox();
+            txtSayfa = new TextBox();
+            txtYazar = new TextBox();
+            txtAd = new TextBox();
+            txtid = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             groupBox2 = new GroupBox();
             dataGridView1 = new DataGridView();
             groupBox3 = new GroupBox();
-            label7 = new Label();
             lblToplamAdet = new Label();
+            label7 = new Label();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -76,102 +78,15 @@
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             // 
-            // label1
+            // btnSil
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(56, 33);
-            label1.Name = "label1";
-            label1.Size = new Size(33, 23);
-            label1.TabIndex = 0;
-            label1.Text = "ID: ";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(52, 67);
-            label2.Name = "label2";
-            label2.Size = new Size(37, 23);
-            label2.TabIndex = 1;
-            label2.Text = "AD: ";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(28, 101);
-            label3.Name = "label3";
-            label3.Size = new Size(61, 23);
-            label3.TabIndex = 2;
-            label3.Text = "YAZAR: ";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(29, 135);
-            label4.Name = "label4";
-            label4.Size = new Size(60, 23);
-            label4.TabIndex = 3;
-            label4.Text = "SAYFA: ";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(8, 169);
-            label5.Name = "label5";
-            label5.Size = new Size(81, 23);
-            label5.TabIndex = 4;
-            label5.Text = "YAYINEVİ: ";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(43, 203);
-            label6.Name = "label6";
-            label6.Size = new Size(46, 23);
-            label6.TabIndex = 5;
-            label6.Text = "TÜR: ";
-            // 
-            // txtid
-            // 
-            txtid.Enabled = false;
-            txtid.Location = new Point(95, 30);
-            txtid.Name = "txtid";
-            txtid.Size = new Size(163, 28);
-            txtid.TabIndex = 6;
-            // 
-            // txtAd
-            // 
-            txtAd.Location = new Point(95, 64);
-            txtAd.Name = "txtAd";
-            txtAd.Size = new Size(163, 28);
-            txtAd.TabIndex = 7;
-            // 
-            // txtYazar
-            // 
-            txtYazar.Location = new Point(95, 98);
-            txtYazar.Name = "txtYazar";
-            txtYazar.Size = new Size(163, 28);
-            txtYazar.TabIndex = 8;
-            // 
-            // txtSayfa
-            // 
-            txtSayfa.Location = new Point(95, 132);
-            txtSayfa.Name = "txtSayfa";
-            txtSayfa.Size = new Size(163, 28);
-            txtSayfa.TabIndex = 9;
-            // 
-            // txtYayinevi
-            // 
-            txtYayinevi.Location = new Point(95, 166);
-            txtYayinevi.Name = "txtYayinevi";
-            txtYayinevi.Size = new Size(163, 28);
-            txtYayinevi.TabIndex = 10;
-            // 
-            // txtTur
-            // 
-            txtTur.Location = new Point(95, 200);
-            txtTur.Name = "txtTur";
-            txtTur.Size = new Size(163, 28);
-            txtTur.TabIndex = 11;
+            btnSil.ForeColor = Color.FromArgb(70, 67, 87);
+            btnSil.Location = new Point(52, 245);
+            btnSil.Name = "btnSil";
+            btnSil.Size = new Size(86, 33);
+            btnSil.TabIndex = 13;
+            btnSil.Text = "SİL";
+            btnSil.UseVisualStyleBackColor = true;
             // 
             // btnEkle
             // 
@@ -183,18 +98,106 @@
             btnEkle.Text = "EKLE";
             btnEkle.UseVisualStyleBackColor = true;
             // 
-            // btnSil
+            // txtTur
             // 
-            btnSil.ForeColor = Color.FromArgb(70, 67, 87);
-            btnSil.Location = new Point(52, 245);
-            btnSil.Name = "btnSil";
-            btnSil.Size = new Size(86, 33);
-            btnSil.TabIndex = 13;
-            btnSil.Text = "SİL";
-            btnSil.UseVisualStyleBackColor = true;
+            txtTur.Location = new Point(95, 200);
+            txtTur.Name = "txtTur";
+            txtTur.Size = new Size(163, 28);
+            txtTur.TabIndex = 11;
+            // 
+            // txtYayinevi
+            // 
+            txtYayinevi.Location = new Point(95, 166);
+            txtYayinevi.Name = "txtYayinevi";
+            txtYayinevi.Size = new Size(163, 28);
+            txtYayinevi.TabIndex = 10;
+            // 
+            // txtSayfa
+            // 
+            txtSayfa.Location = new Point(95, 132);
+            txtSayfa.Name = "txtSayfa";
+            txtSayfa.Size = new Size(163, 28);
+            txtSayfa.TabIndex = 9;
+            // 
+            // txtYazar
+            // 
+            txtYazar.Location = new Point(95, 98);
+            txtYazar.Name = "txtYazar";
+            txtYazar.Size = new Size(163, 28);
+            txtYazar.TabIndex = 8;
+            // 
+            // txtAd
+            // 
+            txtAd.Location = new Point(95, 64);
+            txtAd.Name = "txtAd";
+            txtAd.Size = new Size(163, 28);
+            txtAd.TabIndex = 7;
+            // 
+            // txtid
+            // 
+            txtid.Enabled = false;
+            txtid.Location = new Point(95, 30);
+            txtid.Name = "txtid";
+            txtid.Size = new Size(163, 28);
+            txtid.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(43, 203);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 23);
+            label6.TabIndex = 5;
+            label6.Text = "TÜR: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(8, 169);
+            label5.Name = "label5";
+            label5.Size = new Size(81, 23);
+            label5.TabIndex = 4;
+            label5.Text = "YAYINEVİ: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(29, 135);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 23);
+            label4.TabIndex = 3;
+            label4.Text = "SAYFA: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(28, 101);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 23);
+            label3.TabIndex = 2;
+            label3.Text = "YAZAR: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(52, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(37, 23);
+            label2.TabIndex = 1;
+            label2.Text = "AD: ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 23);
+            label1.TabIndex = 0;
+            label1.Text = "ID: ";
             // 
             // groupBox2
             // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             groupBox2.Controls.Add(dataGridView1);
             groupBox2.Location = new Point(299, 12);
             groupBox2.Name = "groupBox2";
@@ -204,42 +207,64 @@
             // 
             // dataGridView1
             // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Sitka Display", 12F, FontStyle.Italic, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = Color.FromArgb(70, 67, 87);
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Sitka Display", 12F, FontStyle.Italic, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = Color.FromArgb(181, 187, 212);
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             dataGridView1.Dock = DockStyle.Fill;
             dataGridView1.Location = new Point(3, 24);
+            dataGridView1.MultiSelect = false;
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
             dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView1.Size = new Size(410, 223);
             dataGridView1.TabIndex = 0;
             // 
             // groupBox3
             // 
+            groupBox3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             groupBox3.Controls.Add(lblToplamAdet);
             groupBox3.Controls.Add(label7);
-            groupBox3.Location = new Point(302, 265);
+            groupBox3.Location = new Point(302, 268);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(410, 53);
+            groupBox3.Size = new Size(410, 50);
             groupBox3.TabIndex = 2;
             groupBox3.TabStop = false;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(9, 21);
-            label7.Name = "label7";
-            label7.Size = new Size(115, 23);
-            label7.TabIndex = 6;
-            label7.Text = "TOPLAM ADET: ";
             // 
             // lblToplamAdet
             // 
             lblToplamAdet.AutoSize = true;
-            lblToplamAdet.Location = new Point(130, 24);
+            lblToplamAdet.Location = new Point(130, 18);
             lblToplamAdet.Name = "lblToplamAdet";
             lblToplamAdet.Size = new Size(34, 23);
             lblToplamAdet.TabIndex = 7;
             lblToplamAdet.Text = "***";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(9, 18);
+            label7.Name = "label7";
+            label7.Size = new Size(115, 23);
+            label7.TabIndex = 6;
+            label7.Text = "TOPLAM ADET: ";
             // 
             // Form1
             // 
@@ -256,6 +281,7 @@
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Trigger Kullanımı";
+            Load += Form1_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
--- Trigger_Kullanimi/Form1.cs
+++ Trigger_Kullanimi/Form1.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Data.SqlClient;
+
 namespace Trigger_Kullanimi
 {
     public partial class Form1 : Form
@@ -5,6 +8,24 @@ namespace Trigger_Kullanimi
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=krsDbTrigger;Integrated Security=True");
+
+        private void DersListele()
+        {
+            con.Open();
+            string komut = "select * from tbl_books";
+            SqlCommand cmd = new SqlCommand(komut, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DersListele();
         }
     }
 }
